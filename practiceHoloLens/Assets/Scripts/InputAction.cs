7ae379d5861dccb5280770832a767d69a6793cf2
Nisho Matsushita <robonich@mercari.com>
2017-12-18 13:54:59 +0900

    InputAction を追加してタップイベントを受け取れるようになった

---
+++ practiceHoloLens/Assets/Scripts/InputAction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class InputAction : MonoBehaviour,  IInputClickHandler{
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
