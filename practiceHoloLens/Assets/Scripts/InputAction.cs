ac5ea1fe4e2489f82e1cb5abd19a47847c6d9a1b
Nisho Matsushita <robonich@mercari.com>
2018-01-04 09:40:44 +0900

    Update script

--- practiceHoloLens/Assets/Scripts/InputAction.cs
+++ practiceHoloLens/Assets/Scripts/InputAction.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 
-public class InputAction : MonoBehaviour,  IInputClickHandler{
+public class InputAction : MonoBehaviour, IInputClickHandler, IHoldHandler {
+    public void OnHoldCanceled(HoldEventData eventData)
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    public void OnHoldCompleted(HoldEventData eventData)
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void OnHoldStarted(HoldEventData eventData)
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
 
     public void OnInputClicked(InputClickedEventData eventData)
     {
-        GetComponent<Renderer>().material.color = Color.red;
+        //GetComponent<Renderer>().material.color = Color.red;
+        //var rigidbody = gameObject.AddComponent<Rigidbody>();
+        //rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
     }
+
+
+
+
 
     // Use this for initialization
     void Start () {
